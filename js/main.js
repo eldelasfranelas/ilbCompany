e5fafd8c80a7a55afcc8f299e17329a3400aa7a0
anasanchez <anita2395.as@gmail.com>
2019-11-04 23:35:58 -0500

    gallery

--- js/main.js
+++ js/main.js
@@ -332,31 +332,36 @@
 		loaderPage();
 	});
 
-	$(document).ready(function() {
-
-		// required elements
-		var imgPopup = $('.img-popup');
-		var imgCont  = $('.container__img-holder');
-		var popupImage = $('.img-popup img');
-		var closeBtn = $('.close-btn');
-
-		// handle events
-		imgCont.on('click', function() {
-			var img_src = $(this).children('img').attr('src');
-			imgPopup.children('img').attr('src', img_src);
-			imgPopup.addClass('opened');
-		});
-
-		$(imgPopup, closeBtn).on('click', function() {
-			imgPopup.removeClass('opened');
-			imgPopup.children('img').attr('src', '');
-		});
-
-		popupImage.on('click', function(e) {
-			e.stopPropagation();
-		});
-
+	// MDB Lightbox Init
+	$(function () {
+		$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 	});
+
+	// $(document).ready(function() {
+	//
+	// 	// required elements
+	// 	var imgPopup = $('.img-popup');
+	// 	var imgCont  = $('.container__img-holder');
+	// 	var popupImage = $('.img-popup img');
+	// 	var closeBtn = $('.close-btn');
+	//
+	// 	// handle events
+	// 	imgCont.on('click', function() {
+	// 		var img_src = $(this).children('img').attr('src');
+	// 		imgPopup.children('img').attr('src', img_src);
+	// 		imgPopup.addClass('opened');
+	// 	});
+	//
+	// 	$(imgPopup, closeBtn).on('click', function() {
+	// 		imgPopup.removeClass('opened');
+	// 		imgPopup.children('img').attr('src', '');
+	// 	});
+	//
+	// 	popupImage.on('click', function(e) {
+	// 		e.stopPropagation();
+	// 	});
+	//
+	// });
 
 	function isValidEmailAddress(emailAddress) {
 		var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
